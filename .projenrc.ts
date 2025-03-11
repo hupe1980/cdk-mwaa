41ad9d2d2cb9ee71dc750b6d1c8eefa7413ce925
hupe1980 <frankhuebner1980@gmail.com>
2025-03-11 18:23:21 +0000

    Fix repository url

--- .projenrc.ts
+++ .projenrc.ts
@@ -8,7 +8,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   jsiiVersion: '~5.7.0',
   name: 'cdk-mwaa',
   projenrcTs: true,
-  repositoryUrl: 'git@github.com:hupe1980/cdk-mwaa.git',
+  repositoryUrl: 'https://github.com/hupe1980/cdk-mwaa.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
